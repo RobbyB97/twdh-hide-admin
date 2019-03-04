2811bbc06cabb58f219e4416d03dba78eeb2bab3
Robert Bergers <bergersr@my.easternct.edu>
2019-03-04 17:12:37 -0500

    add table layout for settings panel

--- twdh-hide-admin/includes/twdh-hide-admin-settings.php
+++ twdh-hide-admin/includes/twdh-hide-admin-settings.php
@@ -8,7 +8,7 @@ defined ( 'ABSPATH' ) or exit;
 
 $settings = get_option('twdhha_options');
 
-// Check for post
+// Update twdhha_options on post
 if (isset($_POST['submit'])) {
   if (wp_verify_nonce($_POST['_nonce'], 'twdhha_submit')) {
 
@@ -31,6 +31,33 @@ if (isset($_POST['submit'])) {
 ?>
 <html>
   <body>
+    <div class="wrap">
+      <h1 align="left">
+          TWDH Hide Admin
+      </h1>
+      <hr>
+      <form action="" method="POST">
+        <table class="form-table">
+          <tr>
+            <th scope="row" width="50%">
 
+            </th>
+            <td width="50%">
+
+            </td>
+          </tr>
+
+          <tr scope="row">
+            <th scope="row" width="50%">
+
+            </th>
+            <td width="50%">
+
+            </td>
+          </tr>
+        </table>
+      </form>
+
+    </div>
   </body>
 </html>
